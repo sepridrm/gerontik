26efbb4167868f63a5725bd6c793386c495fbb6c
Sepri Dermawan <sepridrm@MacBook-Air.local>
2024-01-19 20:04:51 +0700

    fix error productiom

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import { useFonts } from 'expo-font';
 import Router from './src/router';
 import reducers from './src/reducers/index'
